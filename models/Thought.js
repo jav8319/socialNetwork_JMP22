5c39699dc5eda979faef78b164234bb303e24d78
Arcesio <jav8319@hotmail.com>
2022-07-30 21:35:28 -0400

    thought model modified_formatting date

--- models/Thought.js
+++ models/Thought.js
@@ -18,14 +18,15 @@ const reactionSchema = new Schema(
     },
 
     createdAt: {
-      type: Date,
-      default: Date.now,
+      type: Date, 
+      default: Date.now(),
+      transform: v => v.toLocaleString('en-US')
     },
   },
 
 );
 
-// Schema to create User model
+
 const thoughtSchema = new Schema(
   {
     thoughtText:{
@@ -33,12 +34,13 @@ const thoughtSchema = new Schema(
       required: true,
     },
     
-    
-    createdAt: {
-      type: Date,
-      default: Date.now,
+    date:{
+      type: Date, 
+      default: Date.now(),
+      transform: v => v.toLocaleString('en-US')
+
     },
-   
+
     username:{
       type: String,
       required: true
@@ -47,8 +49,7 @@ const thoughtSchema = new Schema(
     reactions:[reactionSchema]
   },
   {
-    // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
-    // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
+
     toJSON: {
       virtuals: true,
     },
@@ -59,8 +60,6 @@ const thoughtSchema = new Schema(
 
 
 
-
-// Create a virtual property `fullName` that gets and sets the user's full name
 thoughtSchema
   .virtual('reactionCount')
   // Getter
@@ -68,7 +67,7 @@ thoughtSchema
     return this.reactions.length;
   });
 
-// Initialize our User model
+
 const Thought = model('thought', thoughtSchema);
 
 
